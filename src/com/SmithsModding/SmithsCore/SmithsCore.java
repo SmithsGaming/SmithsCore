b2317f37053f550efd557553e3251e0a8c82a0cd
OrionDevelopment <OrionDevelopment@outlook.com>
2015-10-25 21:49:59 +0100

    Initial Commit with Data

---
+++ src/com/SmithsModding/SmithsCore/SmithsCore.java
@@ -0,0 +1,11 @@
+package com.SmithsModding.SmithsCore;
+
+/**
+ * Created by Orion
+ * Created on 25.10.2015
+ * 21:46
+ * <p/>
+ * Copyrighted according to Project specific license
+ */
+public class SmithsCore {
+}
