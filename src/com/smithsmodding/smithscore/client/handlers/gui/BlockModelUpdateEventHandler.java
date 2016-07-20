98d7adabbcc97f7a420707aec5966db5c1a87478
Orion <oriondevelopment@outlook.com>
2016-07-20 20:32:38 +0200

    Changed the way Rerenders are qued on the client.

--- src/com/smithsmodding/smithscore/client/handlers/gui/BlockModelUpdateEventHandler.java
+++ src/com/smithsmodding/smithscore/client/handlers/gui/BlockModelUpdateEventHandler.java
@@ -2,6 +2,7 @@ package com.smithsmodding.smithscore.client.handlers.gui;
 
 import com.smithsmodding.smithscore.client.events.models.block.BlockModelUpdateEvent;
 import com.smithsmodding.smithscore.common.tileentity.IBlockModelUpdatingTileEntity;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -18,6 +19,8 @@ public class BlockModelUpdateEventHandler {
         if (!( entity instanceof IBlockModelUpdatingTileEntity ))
             return;
 
-        entity.getWorld().markChunkDirty(entity.getPos(), entity);
+        IBlockState state = entity.getWorld().getBlockState(event.getBlockPosition().toBlockPos());
+
+        entity.getWorld().notifyBlockUpdate(event.getBlockPosition().toBlockPos(), state, state, 3);
     }
 }
