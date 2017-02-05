879f37c434e80502521cb19ce4cea8d843a42f40
OrionOnline <oriondevelopment@outlook.com>
2017-02-05 19:49:40 +0100

    Changed the Structure mechanics to allow Null structures to exist.
    Signed-off-by: OrionOnline <oriondevelopment@outlook.com>

--- src/com/smithsmodding/smithscore/common/structures/IStructurePart.java
+++ src/com/smithsmodding/smithscore/common/structures/IStructurePart.java
@@ -5,6 +5,7 @@ import com.smithsmodding.smithscore.util.common.positioning.Coordinate3D;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Author Orion (Created on: 25.06.2016)
@@ -14,10 +15,10 @@ public interface IStructurePart<S extends IStructure> extends IPathComponent {
     @Nonnull
     Class<S> getStructureType();
 
-    @Nonnull
+    @Nullable
     S getStructure();
 
-    void setStructure(@Nonnull S structure);
+    void setStructure(@Nullable S structure);
 
     @Nonnull
     World getEnvironment();
